474802e54a8374790f7e39f3b3b412433a1ae700
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-01 08:24:19 +0200

    Excercise n.2 - Let a class become a costant

--- index.php
+++ index.php
@@ -6,9 +6,17 @@ echo "Hello, World!";
 
 // Define a class called Person.
 
+// Excercise Kata n.2
+
+// Since all Persons are of the species "Homo Sapiens", make that a class constant.
+
 class Person
 {
+    const SPECIES = "Homo Sapiens";
     public $name;
     public $age;
-    
+    public $occupation;
 }
+
+
+
