050fc5f662ecb88cfd9d827ac256953ed8f549ee
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-01 08:27:07 +0200

    Excercise n.3 - Declare a class without defining it

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ echo "Hello, World!";
 
 // Since all Persons are of the species "Homo Sapiens", make that a class constant.
 
+// Excercise Kata n.3
+
+// Declare (but do not define yet) the 3 class properties $name, $age and $occupation. *They should all be *public.
+
 class Person
 {
     const SPECIES = "Homo Sapiens";
-    
+    public $name;
+    public $age;
+    public $occupation;
 }
 
 
