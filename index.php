db709cb314b2633156acdfe0cbd3839e506967ea
Frankosviz <geom.francescopaolo@gmail.com>
2024-10-02 18:12:39 +0200

    Fourth Kata

--- index.php
+++ index.php
@@ -182,9 +182,22 @@ function vm($initialVersion = "0.0.1") {
     };
 }
 
+// FOURTH KATA
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
 
 
-
+function replaceDots($str) {
+    return str_replace('.', '-', $str);
+}
 
 
 
